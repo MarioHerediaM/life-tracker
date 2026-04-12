df8788bfce9ae7e494bf17b51cf2ef7e7aa853b7
MarioHerediaM <marioherediamorales@gmail.com>
2026-04-11 20:56:49 -0400

    fix: configuración static files para producción

--- lifetracker/settings.py
+++ lifetracker/settings.py
@@ -109,8 +109,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'tracker/static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
